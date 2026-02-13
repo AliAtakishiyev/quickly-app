95bc4a98e216cca6457ebf147da78bf324492f0d
aliatakishiyev <aliatakishiyev2006@gmail.com>
2026-02-13 13:08:02 +0400

    feat: note provider

---
+++ lib/features/quickly/providers/note_provider.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:quickly_app/features/quickly/data/repositories/note_repository.dart';
+import 'package:quickly_app/features/quickly/models/note_model.dart';
+
+class NoteProvider extends ChangeNotifier {
+  final NoteRepository _repository = NoteRepository();
+
+  List<Note> _notes = [];
+  List<Note> get notes => _notes;
+
+  // Load notes from Hive
+  void loadNotes() {
+    _notes = _repository.getAllNotes();
+    notifyListeners();
+  }
+
+  Future<void> addNote(String title, String content, String summarize) async {
+    await _repository.addNote(
+      title,
+      content,
+      DateTime.now(),
+      DateTime.now(),
+      summarize,
+    );
+
+    loadNotes(); // refresh list
+  }
+
+  Future<void> deleteNote(int hiveId) async {
+    await _repository.deleteNote(hiveId);
+    loadNotes();
+  }
+
+  Future<void> editNote(String title, String content, int hiveId) async {
+    await _repository.editNote(title, content, hiveId);
+    loadNotes();
+  }
+}
